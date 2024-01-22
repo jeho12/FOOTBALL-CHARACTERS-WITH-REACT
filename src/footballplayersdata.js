09d4bb84becc79b89368a1f785fa666edeaf50b5
Ibenye Jehoshaphat <jehoibenye@gmail.com>
2024-01-22 10:46:15 -0800

    football

---
+++ src/footballplayersdata.js
@@ -0,0 +1,138 @@
+export const characters = [
+  // football players 
+  {
+    name: "",
+    alias: "Thiago Silver",
+    img1:"./player 17.png",
+    //  "https://www.specfictionshop.com/cdn/shop/products/315455127_2253071438203857_6311282012262232749_n_2000x.jpg?v=1669836598",
+    homeworld: "Earth",
+    abilities: [
+      "Genius-level intellect",
+      "Powered armor suit",
+      "Superhuman strength",
+    ],
+    team: "Soldiers of Christ",
+    description:
+      "Jeho himself, also known as Jehoshaphat, is a brilliant inventor, philanthropist, and industrialist. He uses his genius-level intellect and a highly advanced powered armor suit to fight for justice and protect the world from various threats. Stark is a charismatic playboy turned superhero who combines his technological prowess with unwavering determination to uphold the values of heroism and innovation.",
+  },
+  {
+    name: "Ibenye Joshua",
+    alias: "Sterling",
+    abilities: ["Powered armor suit", "Advanced technology skills"],
+    team: "APPLAUDE",
+     img1:"./player 13.png",
+    // "https://i.pinimg.com/originals/5e/fe/55/5efe55d2a7785a311511736a1a37bcbb.jpg",
+    homeworld: "Earth",
+    description:
+      "Pepper Potts, the formidable Rescue, is an accomplished businesswoman and technological genius. She dons a powered armor suit equipped with an array of advanced technology and weaponry, allowing her to stand alongside the Avengers in the battle against supervillains and global threats. Pepper is a determined and fearless heroine with an unwavering commitment to justice.",
+  },
+  {
+    name: "James 'Rhodey' Rhodes",
+    alias: "Reece James",
+    abilities: ["Powered armor suit", "Military expertise"],
+    team: "Avengers",
+    img1: "./player 14.png",
+    homeworld: "Earth",
+
+    description:
+      "James 'Rhodey' Rhodes, also known as War Machine, is a highly skilled pilot and a military expert. He operates a heavily armed powered armor suit that enhances his strength and combat capabilities. With a strong sense of duty and loyalty to his allies, Rhodey plays a crucial role in the Avengers' efforts to maintain peace and protect the world.",
+  },
+  {
+    name: "Riri Williams",
+    alias: "Eden Harzard",
+    abilities: ["Powered armor suit", "Genius-level intellect"],
+    team: "Young Avengers",
+    img1: "./player 9.png",
+    homeworld: "Earth",
+
+    description:
+      "Riri Williams, known as Ironheart, is a prodigious young inventor with a genius-level intellect. She has designed her own advanced powered armor suit, which she uses to fight against threats as a member of the Young Avengers. Riri's intelligence, determination, and innovative spirit make her a rising star in the superhero community.",
+  },
+
+  // Spider-Man
+  {
+    name: "Miles Morales",
+    alias: "Enzo-Fernandez",
+    abilities: ["Bio-electrokinesis", "Wall-crawling"],
+    team: "Champions",
+    img1: "./player 5.png",
+    homeworld: "Earth",
+    img3: "url_to_img3_for_Natasha_Stark",
+    description:
+      "Miles Morales, the teenage Spider-Man, has bio-electrokinesis powers, in addition to wall-crawling abilities. As a member of the Champions, Miles embodies the spirit of the Spider-Man legacy, fighting for equality and justice in the Marvel Universe while navigating the challenges of being a young hero.",
+  },
+
+  {
+    name: "Cindy Moon",
+    alias: "Mason Burstow",
+    abilities: ["Web-shooting", "Superhuman agility"],
+    team: "Agents of Atlas",
+    img1: "./player 7.png",
+    homeworld: "Earth",
+    description:
+      "Cindy Moon, codenamed Silk, is a skilled hero with the power of web-shooting and remarkable superhuman agility. She is an essential member of the Agents of Atlas, using her arachnid-like abilities to combat threats and safeguard the world.",
+  },
+
+  {
+    name: "Steve Rogers",
+    alias: "Noni-Madueke",
+    abilities: ["Peak human strength", "Vibranium shield"],
+    team: "Avengers",
+    img1: "./player 8.png",
+    homeworld: "Earth",
+    description:
+      "Steve Rogers, Captain America, is the embodiment of honor and virtue. Enhanced with peak human strength and wielding a Vibranium shield, he stands as a symbol of justice and resilience. Steve's unwavering commitment to the ideals of heroism makes him a cornerstone of the Avengers and the Marvel Universe.",
+  },
+  {
+    name: "Sam Wilson",
+    alias: "Kalidou Koulibaly",
+    abilities: ["Wingsuit", "Telepathic link with birds"],
+    team: "Avengers",
+    img1: "./player 6.png",
+    homeworld: "Earth",
+    description:
+      "Sam Wilson, the Falcon, soars through the skies with his high-tech wingsuit and possesses a unique telepathic link with birds. As an Avenger, Sam exemplifies freedom, hope, and unity, fighting alongside his fellow heroes to maintain peace and protect the world from impending threats.",
+  },
+  {
+    name: "LIPSON",
+    alias: "Mykhailo Mudryk",
+    abilities: ["Wingsuit", "Telepathic link with birds"],
+    team: "Avengers",
+    img1: "./player 10.png",
+    homeworld: "Earth",
+    description:
+      "Sam Wilson, the Falcon, soars through the skies with his high-tech wingsuit and possesses a unique telepathic link with birds. As an Avenger, Sam exemplifies freedom, hope, and unity, fighting alongside his fellow heroes to maintain peace and protect the world from impending threats.",
+  },
+  {
+    name: "LIPSON",
+    alias: "Nicolas Jackson",
+    abilities: ["Wingsuit", "Telepathic link with birds"],
+    team: "Avengers",
+    img1: "./player 11.png",
+    homeworld: "Earth",
+    description:
+      "Sam Wilson, the Falcon, soars through the skies with his high-tech wingsuit and possesses a unique telepathic link with birds. As an Avenger, Sam exemplifies freedom, hope, and unity, fighting alongside his fellow heroes to maintain peace and protect the world from impending threats.",
+  },
+  {
+    name: "LIPSON",
+    alias: "Robert Sánchez",
+    abilities: ["Wingsuit", "Telepathic link with birds"],
+    team: "Avengers",
+    img1: "./player 16.png",
+    homeworld: "Earth",
+    description:
+      "Sam Wilson, the Falcon, soars through the skies with his high-tech wingsuit and possesses a unique telepathic link with birds. As an Avenger, Sam exemplifies freedom, hope, and unity, fighting alongside his fellow heroes to maintain peace and protect the world from impending threats.",
+  },
+  {
+    name: "LIPSON",
+    alias: "Gallagher",
+    abilities: ["Wingsuit", "Telepathic link with birds"],
+    team: "Avengers",
+    img1: "./player 3.png",
+    homeworld: "Earth",
+    description:
+      "Sam Wilson, the Falcon, soars through the skies with his high-tech wingsuit and possesses a unique telepathic link with birds. As an Avenger, Sam exemplifies freedom, hope, and unity, fighting alongside his fellow heroes to maintain peace and protect the world from impending threats.",
+  },
+];
+
+console.log(characters);
